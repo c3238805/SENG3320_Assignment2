6b5753803932f76937e81acdd39a2bbea52d5f41
Brandon Allen <brandon.allen263@gmail.com>
2022-05-19 13:35:27 +1000

    Update Triangle.c

--- symbolic_execution/Triangle.c
+++ symbolic_execution/Triangle.c
@@ -1,4 +1,7 @@
+#include <stdio.h>
+
 void triangle (int a, int b, int c){
+    printf("%d, %d, %d", a, b, c);
     if ((a+b>c)&&(a+c>b)&&(b+c>a)) {
         if (a==b || a==c || b==c) {
             if (a==c && a==b)
