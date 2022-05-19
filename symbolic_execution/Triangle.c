4cf2c9d9c164c52c41f69eb6fd55325a989f2e56
Brandon Allen <brandon.allen263@gmail.com>
2022-05-19 14:14:33 +1000

    Draft portion of report for control flow analysis

--- symbolic_execution/Triangle.c
+++ symbolic_execution/Triangle.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 void triangle (int a, int b, int c){
-    printf("%d, %d, %d", a, b, c);
     if ((a+b>c)&&(a+c>b)&&(b+c>a)) {
         if (a==b || a==c || b==c) {
             if (a==c && a==b)
