dabc1f38ab27c505c6b51313d207c47d21416310
Kyle12x <91297722+Kyle12x@users.noreply.github.com>
2022-05-19 16:07:28 +1000

    Added Q1 Files

---
+++ Q1/RandomData.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.Random;
+ 
+public class RandomData
+{
+   public static void generateRandomData(int numberOfFiles, int maxLines, int maxCharacter)
+   {
+      Random rand = new Random();
+
+      int lines;
+      int numberOfCharacters;
+      String lineString;
+
+      for (int i = 1; i <= numberOfFiles; i++) 
+      {
+         lines = rand.nextInt(maxLines) + 1;  
+         
+         try
+         {
+            File inputText = new File(String.format("InputTextFolder//inputText" +"%05d" +".txt",i));
+            inputText.createNewFile();
+
+            FileWriter writeFile = new FileWriter(inputText, false);
+
+            for(int j = 0; j < lines; j++)
+            {
+               numberOfCharacters = rand.nextInt(maxCharacter) + 1;
+               lineString = generateString(numberOfCharacters);
+               writeFile.write(lineString);
+            }
+            writeFile.close();
+         }
+         catch(Exception e)
+         {
+            System.out.println("An error occurred: e");
+            e.printStackTrace();
+         }
+
+      }
+   }
+
+   private static String generateString(int characters)
+   {
+      Random rand = new Random();
+      int asciiNumber;
+      String lineString = "";
+
+      for(int i = 0; i < characters; i++)
+      {
+         asciiNumber = rand.nextInt(94) + 32;
+         lineString += Character.toString(asciiNumber);
+      }
+
+      return lineString + "\n";
+   }
+}
