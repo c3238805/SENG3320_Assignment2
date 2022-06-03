728c81c9e66e7baa3561bdfe9600d886cee75d41
Kyle12x <91297722+Kyle12x@users.noreply.github.com>
2022-06-03 16:41:09 +1000

    Added comments to Q1

--- Q1/ExceptionHandler.java
+++ Q1/ExceptionHandler.java
@@ -1,3 +1,12 @@
+// KWICTester.java
+// **************************
+//
+// Written by   : Kyle Beattie c3303374 Group10
+// Course       : SENG3320
+// Modified     : 03/06/2022
+// 
+// Program Description: Handles the exceptions generated from the KWIC program so that only unique exceptions are stored and printed 
+
 import java.io.*;
 import java.util.*;
 
@@ -9,20 +18,31 @@ public class ExceptionHandler
 
    public ExceptionHandler()
    {
+      // Holds the entire exception string
       exceptionSet = new LinkedHashSet<String>();
+
+      // Holds the exception values after the first "at"
       subStringSet = new LinkedHashSet<String>();
+
+      // Holds the name of the input file that caused the exception
       inputSet = new LinkedList<String>();
    }
 
    public void addException(String exception, String inputText)
    {
+      // Checks if the exception contains "at"
       if(exception.contains("at"))
       {
+         // Splits the exception string at the first index of "at"
          String exceptionSubString = exception.substring(exception.indexOf("at"));
+
+         // Checks if the exception message after the at is already present so only unique exceptions are stored
          if(subStringSet.add(exceptionSubString))
          {
+            // Double checks if the entire exception message has been saved
             if(exceptionSet.add(exception))
             {
+               // Adds the input text file name
                inputSet.add(inputText); 
             }
          }
@@ -33,25 +53,32 @@ public class ExceptionHandler
    {
       try
       {
+         // Creates a new exceptions.txt file to contain the exception information
          File f2 = new File("ExceptionTextFolder//exceptions.txt");
          f2.createNewFile();
          FileWriter writeFile = new FileWriter(f2, false);
 
+         // Creates an iterator for the input and exception sets
          Iterator<String> inputIterator = inputSet.iterator();
          Iterator<String> exceptionIterator = exceptionSet.iterator();
 
          int i = 0;
 
          writeFile.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\r");
+
+         // Loops through until all exceptions have been finished printing to the exception text file
          while(inputIterator.hasNext() && exceptionIterator.hasNext())
          {
+            // Retrieves the exception information 
             String inputText = inputIterator.next();
             String exception = exceptionIterator.next();
+
+            // Counts the number of unique exceptions
             i++;
 
+            // Writes each line of the input text file associated with the caught exception to the exceptions.txt file
             File inputFile = new File("InputTextFolder//" + inputText);
             Scanner myScanner = new Scanner(inputFile);
-
             writeFile.write("Input: " + inputText + "\r");
             
             while(myScanner.hasNextLine())
@@ -61,13 +88,13 @@ public class ExceptionHandler
 
             myScanner.close();
 
+            // Writes the exception caught to the exceptions.txt file
             writeFile.write("\rException: \r" + exception);
             writeFile.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\r");
-
-            File inputTextFile = new File("InputTextFolder//" + inputText);
          }
-         writeFile.write("Unique Exceptions: " + i);
 
+         // Writes the number of unique exceptions generated from Fuzz Testing
+         writeFile.write("Unique Exceptions: " + i);
          writeFile.close();
 
          System.out.println("\nUnique Exceptions: " + i);
